e17562202d7aa53fa947d31d92c82760c583888c
Darren Thong <conkehlicious@gmail.com>
2024-08-01 12:42:14 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,31 +1,34 @@
-// recipe data
-const recipe = {
-  title: "Spaghetti Carbonara",
-  instructions:
-    "1. Cook spaghetti according to package instructions. 2. In a separate pan, cook pancetta until crispy. 3. In a bowl, whisk eggs and grated cheese. 4. Combine spaghetti, pancetta, and egg mixture, stirring quickly. 5. Serve immediately with additional cheese and black pepper.",
-  image: "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
-};
-
 // button onclick declared
 getRecipeBtn.onclick = onRandomRecipe;
 
+//  declaration
+const container = document.querySelector(".recipe-container");
+const recipeImage = document.querySelector(".recipe-image");
+
+// api url
+const url = "https://www.themealdb.com/api/json/v1/1/random.php";
+
 // function for onclick button to get recipe
-function onRandomRecipe() {
-  // function for style backgroundcolor
-  document.body.style.backgroundColor = "lightblue";
+async function onRandomRecipe() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data);
 
-  document.querySelector(".container").style.backgroundColor = "lightgrey";
+    // html styling
+    document.body.style.backgroundColor = "black";
+    container.style.width = "700px";
+    container.style.padding = "10px";
+    container.style.minHeight = "500px";
 
-  // function for style image and container
-  document.querySelector(".container").style.width = "400px";
-  document.querySelector(".recipe-image").src = recipe.image;
-  document.querySelector(".recipe-image").style.width = "200px";
-
-  // function for display recipe data
-  document.querySelector(".recipe-title").textContent = recipe.title;
-  document.querySelector(".recipe-instructions").textContent =
-    recipe.instructions;
-
-  // function for hide the button
-  document.getElementById("getRecipeBtn").style.display = "none";
+    // display random recipe
+    container.innerHTML = ` 
+    <h1>${data.meals[0].strMeal}</h1>
+    <h3>${data.meals[0].strCategory}</h3>
+    <p>${data.meals[0].strInstructions} </p>
+    <img style = "width : 200px " src = ${data.meals[0].strMealThumb}>
+    `;
+  } catch (error) {}
 }
+
+onRandomRecipe();
