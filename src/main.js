f2bb09d301c9b8f4d115efc33189c904d0de05f7
Darren Thong <conkehlicious@gmail.com>
2024-08-05 00:15:48 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ getRecipeBtn.onclick = onRandomRecipe;
 
 //  declaration
 const container = document.querySelector(".recipe-container");
+const ingredientText = document.querySelector(".ingredienttext");
 
 // api url
 const url = "https://www.themealdb.com/api/json/v1/1/random.php";
@@ -14,6 +15,19 @@ async function onRandomRecipe() {
     const data = await response.json();
     // console.log(data);
 
+    // ini adalah fungsi loop untuk ingredients
+    const meal = data.meals[0];
+    // console.log(meal);
+    const ingredientss = [];
+    for (let i = 1; i <= 20; i++) {
+      if (meal[`strIngredient${i}`]) {
+        ingredientss.push(`${meal[`strIngredient${i}`]}`);
+      } else {
+        break;
+      }
+    }
+    // console.log(ingredientss);
+
     // html styling
     document.body.style.backgroundColor = "black";
     container.style.width = "350px";
@@ -22,13 +36,18 @@ async function onRandomRecipe() {
 
     // display random recipe
     container.innerHTML = ` 
-    <h1>${data.meals[0].strMeal}</h1>
+    <h2>${data.meals[0].strMeal}</h2>
     <h3>CATEGORY : ${data.meals[0].strCategory}</h3>
     <h3>ORIGIN COUNTRY : ${data.meals[0].strArea}</h3>
     <h3 style = "color : red">INSTRUCTION :</h3>
     <p> ${data.meals[0].strInstructions} </p>
     <img style = "width : 200px " src = ${data.meals[0].strMealThumb}>
     `;
+    ingredientText.innerHTML = `<ol>${ingredientss
+      .map((ingredientss) => `<li>${ingredientss}</li>`)
+      .join("")}</ol>`;
+    country.textContent = `country :${data.meals[0].strArea}`;
+    console.log(ingredientss);
   } catch (error) {}
 }
 
